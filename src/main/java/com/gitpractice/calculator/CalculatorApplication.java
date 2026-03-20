9d6d7cce293cc92943480b447d42600909b13cfc
Kumar Ankesh <kumarankesh1894@gmail.com>
2026-03-20 15:17:57 +0530

    Added new calculator logic in ankesh branch

--- src/main/java/com/gitpractice/calculator/CalculatorApplication.java
+++ src/main/java/com/gitpractice/calculator/CalculatorApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
+	public static int add(int a, int b){
+		return a+b;
+	}
+
 	public static void main(String[] args) {
+		System.out.println(add(2,3));
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
