6d2e5921712808205d3ee2a9f8071892f4bb394a
Tamanna12216506 <tamannayadav741@gmail.com>
2026-03-20 15:24:16 +0530

    Add multiply logic

--- src/main/java/com/gitpractice/calculator/CalculatorApplication.java
+++ src/main/java/com/gitpractice/calculator/CalculatorApplication.java
@@ -8,6 +8,12 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+		System.out.println(multiply(5,20));
 	}
+
+	public static int multiply(int a,int b){
+		return a*b;
+	}
+
 
 }
